9c1eebf8e219c741d1bdbea0e5264625420a7346
admin <D.Alavaro@gmail.com>
2023-07-16 18:16:22 +0300

    step 1

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,44 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+
+class NodeTestCase(unittest.TestCase):
+    def test_node_creation(self):
+        n1 = Node(5, None)
+        n2 = Node('a', n1)
+
+        self.assertEqual(n1.data, 5)
+        self.assertEqual(n2.data, 'a')
+
+class StackTestCase(unittest.TestCase):
+    def test_stack_push(self):
+        stack = Stack()
+
+        stack.push('data1')
+        self.assertEqual(stack.top.data, 'data1')
+
+        stack.push('data2')
+        self.assertEqual(stack.top.data, 'data2')
+        self.assertEqual(stack.top.next_node.data, 'data1')
+
+        stack.push('data3')
+        self.assertEqual(stack.top.data, 'data3')
+        self.assertEqual(stack.top.next_node.data, 'data2')
+        self.assertEqual(stack.top.next_node.next_node.data, 'data1')
+
+    def test_stack_pop(self):
+        stack = Stack()
+
+        self.assertIsNone(stack.pop())
+
+        stack.push('data1')
+        stack.push('data2')
+        stack.push('data3')
+
+        self.assertEqual(stack.pop(), 'data3')
+        self.assertEqual(stack.top.data, 'data2')
+        self.assertEqual(stack.pop(), 'data2')
+        self.assertEqual(stack.top.data, 'data1')
+        self.assertEqual(stack.pop(), 'data1')
+        self.assertIsNone(stack.top)
+        self.assertIsNone(stack.pop())
